6ee1c4ed80bb3e5512156ad6e9dbf72df328fc6a
perlh <jeff.craton@gmail.com>
2017-03-29 22:10:57 -0400

    check in save and trash changes, along with tool clicks and forms for pug

--- NotesExpress1/public/javascripts/viewnote/viewnote.js
+++ NotesExpress1/public/javascripts/viewnote/viewnote.js
@@ -3,11 +3,25 @@
 //
 
 $(document).ready(function () {
+    $(document).tooltip();
+
     //
     // execute after the page has been loaded
     //
     var quill = new Quill('#editor', {
         theme: 'snow'
-    }); +
-
+    }); 
+    //
+    // style buttons
+    //
+    $("#icoSave").click(function () {
+        //alert("Handler for save.click() called.");
+    });
+    $("#icoTrash").click(function () {
+        //alert("Handler for trash.click() called.");
+    });
+    $("#currentdate").text(new Date());
+    //
+    // how do I tell if this is a new note?
+    //
 });
