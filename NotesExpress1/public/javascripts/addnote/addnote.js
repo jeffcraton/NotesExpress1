ea7f176c3bc9a4acc5deca8a1b7bb50d4b34bbd0
perlh <jeff.craton@gmail.com>
2017-04-07 16:28:54 -0400

    committing note post changes. subject and body are not populating in mongodb. looks like its just creating empty entries.

--- NotesExpress1/public/javascripts/addnote/addnote.js
+++ NotesExpress1/public/javascripts/addnote/addnote.js
@@ -15,6 +15,23 @@ $(document).ready(function () {
     // style buttons
     //
     $("#icoSave").click(function () {
-        alert("Handler for save.click() called.");
+        var vSubject = "Test subject information";
+        var vBody = "this is test data";
+        // Assign handlers immediately after making the request,
+        // and remember the jqxhr object for this request
+        var jqxhr = $.post("/notesapi", { subject: vSubject, body: vBody }).done(function () {
+                alert("second success");
+        }).fail(function () {
+                alert("error");
+        }).always(function () {
+            alert("finished");
+        });
+
+        // Perform other work here ...
+
+        // Set another completion function for the request above
+        jqxhr.always(function () {
+            alert("second finished");
+        });
     });
 });
